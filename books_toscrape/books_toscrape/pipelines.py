0a3a585c22da42baf7248e7821d14292685d23e2
Tanmay <tanmay.milky33@gmail.com>
2020-12-30 16:34:20 +0530

    updated pipelines

--- books_toscrape/books_toscrape/pipelines.py
+++ books_toscrape/books_toscrape/pipelines.py
@@ -11,13 +11,12 @@ from scrapy import Request
 
 
 class BooksToscrapePipeline(ImagesPipeline):
-    def process_item(self, item, spider):
-        return item
+
 
     def get_media_requests(self, item, info):
         urls = ItemAdapter(item).get(self.images_urls_field, [])
         return [Request(u, meta={'bookname': item.get('book_name')}) for u in urls]
 
     def file_path(self, request, response=None, info=None, *, item=None):
-        image_guid = hashlib.sha1(to_bytes(request.url)).hexdigest()
+
         return f'full/{request.meta["bookname"]}.jpg'
